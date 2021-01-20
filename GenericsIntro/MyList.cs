b17d95c52f06ee3db552bcb3b5597bfb789c90c1
YAŞAR <YAAR@DESKTOP-NPJQQAG>
2021-01-20 23:50:42 +0300

    20Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempeArray = items;
+            items = new T[items.Length + 1];
+            for (int i = 0; i < tempeArray.Length; i++)
+            {
+                items[i] = tempeArray[i];
+            }
+
+
+            items[items.Length - 1] = item;
+        }
+    }
+}
